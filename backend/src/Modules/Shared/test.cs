e8676dd1cfb55a3fbc2c95399276f0c9be4cce4c
unknown <billelmh501@gmail.com>
2026-08-06 15:13:47 +0100

    create test file

--- backend/src/Modules/Shared/test.cs
+++ backend/src/Modules/Shared/test.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Builder;
 using Modules.Shared.Endpoints;
+using Modules.Shared.Infrastructure.Outbox;
 using Modules.Shared.Infrastructure.Persistence;
 
 public static class Test
@@ -7,8 +9,9 @@ public static class Test
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("test/outbox", (SharedDbContext) => { 
-                  var outboxMessages = SharedDbContext.Set<Outbox>
+            app.MapGet("test/outbox", (SharedDbContext dbContext) => { 
+                  var outboxMessages = dbContext.Set<OutboxMessage>().ToList();
+                  return outboxMessages;
                 });
         }
     }
